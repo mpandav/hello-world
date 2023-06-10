f9a5625a4a9a966903113424dd7b42fc0440f744
mpandav <mpandav32@gmail.com>
2023-06-10 18:19:14 +0200

    initial commit

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public  class helloworld 
+ {
+ 
+       public  static  void main ( String [ ] args ) 
+       { 
+             // output Hello World! 
+             System.out.println ( "Hello World!" ) ; 
+       } 
+}
